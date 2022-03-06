ea3eac013b4f626175e796dda38c4d0be12267e8
Lilia Dimitrova <liliadimitrova36@gmail.com>
2022-03-06 14:17:37 +0200

    Add SayHelloExtend_04 proj to Lectures > InterfacesAndAbstraction

---
+++ JavaOOP/Lectures/InterfacesAndAbstraction/SayHelloExtend_04/src/com/softuni/training/Bulgarian.java
@@ -0,0 +1,13 @@
+package com.softuni.training;
+
+public class Bulgarian extends BasePerson{
+
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
