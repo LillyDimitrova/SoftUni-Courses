5a2a8f6d7bc537d3087539cf867e65557fa9fe8b
Lilia Dimitrova <liliadimitrova36@gmail.com>
2022-03-09 20:52:00 +0200

    Add Animals_03 to Lectures > Polymorphism

---
+++ JavaOOP/Lectures/Polymorphism/Animals_03/Cat.java
@@ -0,0 +1,12 @@
+public class Cat extends Animal{
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    String explainSelf() {
+        return String.format("I am %s and my favourite food is %s\n" +
+                "MEEOW", super.getName(), super.getFavouriteFood());
+    }
+}
---
+++ JavaOOP/Lectures/Polymorphism/Animals_03/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Animal{
+
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    String explainSelf() {
+        return String.format("I am %s and my favourite food is %s\n" +
+                "DJAAF", super.getName(), super.getFavouriteFood());
+    }
+}
